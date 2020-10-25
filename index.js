4cf6eabfa341586d0d5ab1b1b276df642f864262
devzero-deploys <72330074+devzero-deploys@users.noreply.github.com>
2020-10-25 15:41:51 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const cors  =  require('cors');
+const { DOOR } = require('./configs/constants');
+const routes = require('./configs/routes');
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(DOOR, _ => console.log(`Server running on door: ${DOOR}`));
